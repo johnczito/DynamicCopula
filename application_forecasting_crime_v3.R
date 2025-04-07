1862616a6e11afc67dd7584c1b7d7d99206b330e
John Zito <johnczito@gmail.com>
2025-04-07 05:20:33 -0400

    add Poisson simulation

---
+++ application_forecasting_crime_v3.R
@@ -0,0 +1,289 @@
+# ==============================================================================
+# load resources
+# ==============================================================================
+
+source("_packages.R")
+source("_helpers/_helpers.R")
+
+set.seed(8675309)
+
+# ==============================================================================
+# load full data
+# https://www.bocsar.nsw.gov.au/Pages/bocsar_datasets/Offence.aspx
+# ==============================================================================
+
+crime_data = read.csv("datasets/full_nsw_crime_dataset.csv", header = TRUE)
+
+# ==============================================================================
+# subset of data for forecasting
+# ==============================================================================
+
+dates <- as.Date(crime_data$date, format = "%m/%d/%Y")
+
+var_names <- c("murder", "attempted", "manslaughter")
+
+my_labs <- c("Murder", "Attempted", "Manslaughter")
+
+Y <- as.matrix(crime_data[, var_names])
+
+T <- nrow(Y)
+n <- ncol(Y)
+
+
+# ==============================================================================
+# sampling settings
+# ==============================================================================
+
+pois_ndraw = 1000
+nbinom_ndraw = 1000
+
+bvar_ndraw = 1000
+bvar_nburn = 1000
+bvar_nthin = 1
+
+dfc_ndraw  = 1000
+dfc_nburn = 0
+dfc_nthin = 1
+
+# ==============================================================================
+# forecast settings
+# ==============================================================================
+
+H = 5
+Tstart = 12 * 5
+Tstop = T
+
+# ==============================================================================
+# model settings
+# ==============================================================================
+
+bvar_lag = 1
+factor_dim = 3
+
+col_bvar = "orange"
+col_dfc = "blue"
+col_pois = "red"
+col_nbinom = "darkgreen"
+
+# ==============================================================================
+# preallocate storage
+# ==============================================================================
+
+fcast_bvar = array(0, c(H, n, bvar_ndraw, T))
+fcast_dfc  = array(0, c(H, n, dfc_ndraw, T))
+fcast_pois = array(0, c(H, n, pois_ndraw, T))
+fcast_nbinom  = array(0, c(H, n, nbinom_ndraw, T))
+
+# ==============================================================================
+# which model to run
+# ==============================================================================
+
+run_BVAR = TRUE
+run_DGFC = TRUE
+run_POISSON = TRUE
+run_NBINOM = TRUE
+
+# ==============================================================================
+# run it hot!
+# ==============================================================================
+
+for(t in Tstart:Tstop){
+  # ---------------------------
+  # Poisson DGLM
+  # ---------------------------
+  
+  if(run_POISSON){
+    mystart <- Sys.time()
+    
+    fcast_pois[, , , t] <- get_poisson_mvforecasts(Y[1:t, ], H, pois_ndraw)
+    
+    myend <- Sys.time()
+    
+    print(myend - mystart)
+  }
+  
+  # ---------------------------
+  # Negative binomial DGLM
+  # ---------------------------
+  
+  if(run_NBINOM){
+    mystart <- Sys.time()
+    
+    fcast_nbinom[, , , t] <- get_nbinom_mvforecasts(Y[1:t, ], H, nbinom_ndraw)
+    
+    myend <- Sys.time()
+    
+    print(myend - mystart)
+  }
+  
+  # ---------------------------
+  # BVAR
+  # ---------------------------
+  
+  if(run_BVAR){
+    mystart <- Sys.time()
+    
+    my_bvar_model <- bvar(Y[1:t, ], lags = bvar_lag, verbose = FALSE,
+                          n_draw = bvar_ndraw * bvar_nthin + bvar_nburn, n_burn = bvar_nburn, n_thin = bvar_nthin)
+    fcast_bvar[, , , t] <- aperm(predict(my_bvar_model, horizon = H)$fcast, c(2, 3, 1))
+    
+    myend <- Sys.time()
+    
+    print(myend - mystart)
+  }
+  
+  # ---------------------------
+  # BVAR
+  # ---------------------------
+  
+  if(run_DGFC){
+    mystart <- Sys.time()
+    
+    draws = DGFC.mcmc(Y[1:t, ], k.star = factor_dim, 
+                      ndraw = dfc_ndraw, burn = dfc_nburn, thin = dfc_nthin)
+    fcast_dfc[, , , t] = DGFC.forecast(H, draws)
+    
+    myend <- Sys.time()
+    
+    print(myend - mystart)
+  }
+  
+  message(paste("Stage ", t, " of ", Tstop, " done!", sep = ""))
+}
+
+# ==============================================================================
+# h-step-ahead forecast table
+# ==============================================================================
+
+alpha = 0.05
+period = (Tstart + H):Tstop
+h = 1
+metrics = c("MAE", "INT-COV", "INT-SIZE", "CRPS")
+nmod = 4
+nmet = length(metrics)
+mastertable = matrix(0, 0, nmet)
+colnames(mastertable) = metrics
+
+for(i in 1:n){
+  # T x H x ndraw
+  dfc_stuff = get_variable_fcasts(i, fcast_dfc)
+  var_stuff = get_variable_fcasts(i, fcast_bvar)
+  pois_stuff = get_variable_fcasts(i, fcast_pois)
+  nbinom_stuff = get_variable_fcasts(i, fcast_nbinom)
+  
+  # H x 6 x A x T
+  dfc_metrics = get_forecast_metrics(Y[period, i], dfc_stuff[period, , ], alpha)
+  var_metrics = get_forecast_metrics(Y[period, i], var_stuff[period, , ], alpha)
+  pois_metrics = get_forecast_metrics(Y[period, i], pois_stuff[period, , ], alpha)
+  nbinom_metrics = get_forecast_metrics(Y[period, i], nbinom_stuff[period, , ], alpha)
+  
+  # H x 6 x A
+  dfc_results = apply(dfc_metrics, c(1, 2, 3), FUN = mean)
+  var_results = apply(var_metrics, c(1, 2, 3), FUN = mean)
+  pois_results = apply(pois_metrics, c(1, 2, 3), FUN = mean)
+  nbinom_results = apply(nbinom_metrics, c(1, 2, 3), FUN = mean)
+  
+  summ <- round(rbind(dfc_results[h, metrics, 1], var_results[h, metrics, 1], 
+                      pois_results[h, metrics, 1], nbinom_results[h, metrics, 1]), digits = 2)
+  rownames(summ) <- c("DGFC", "BVAR", "Pois-DGLM", "NB-DGLM")
+  
+  mastertable <- rbind(mastertable, summ)
+}
+
+#mastertable <- cbind(mastertable[1:(nmod * n / 2), ], mastertable[((nmod * n / 2) + 1):(nmod*n), ])
+
+#write.csv(mastertable, "crime_fcasts.csv", row.names = FALSE)
+
+my_metric = "MAE"
+
+plot(1:length(period), dfc_metrics[h, my_metric, 1, ], type = "l")
+lines(1:length(period), var_metrics[h, my_metric, 1, ])
+lines(1:length(period), pois_metrics[h, my_metric, 1, ])
+
+
+# ==============================================================================
+# inspect forecasts
+# ==============================================================================
+
+i = 1
+alpha = c(0.01, 0.05, 0.1, 0.25, 0.5)
+period = (Tstart + H):Tstop
+h = 1
+metric = "INT-SIZE"
+
+# T x H x ndraw
+dfc_stuff = get_variable_fcasts(i, fcast_dfc)
+var_stuff = get_variable_fcasts(i, fcast_bvar)
+pois_stuff = get_variable_fcasts(i, fcast_pois)
+nbinom_stuff = get_variable_fcasts(i, fcast_nbinom)
+
+# H x 6 x A x T
+dfc_metrics = get_forecast_metrics(Y[period, i], dfc_stuff[period, , ], alpha)
+var_metrics = get_forecast_metrics(Y[period, i], var_stuff[period, , ], alpha)
+pois_metrics = get_forecast_metrics(Y[period, i], pois_stuff[period, , ], alpha)
+nbinom_metrics = get_forecast_metrics(Y[period, i], nbinom_stuff[period, , ], alpha)
+
+# H x 6 x A
+dfc_results = apply(dfc_metrics, c(1, 2, 3), FUN = mean)
+var_results = apply(var_metrics, c(1, 2, 3), FUN = mean)
+pois_results = apply(pois_metrics, c(1, 2, 3), FUN = mean)
+nbinom_results = apply(nbinom_metrics, c(1, 2, 3), FUN = mean)
+
+par(mar = c(2, 2, 2, 2))
+
+dfc_mse = dfc_results[1:H, metric, 1]
+var_mse = var_results[1:H, metric, 1]
+pois_mse = pois_results[1:H, metric, 1]
+nbinom_mse = nbinom_results[1:H, metric, 1]
+
+L = min(dfc_mse, var_mse, pois_mse, nbinom_mse)
+U = max(dfc_mse, var_mse, pois_mse, nbinom_mse)
+
+plot(1:H, dfc_mse, type = "l", col = col_dfc, ylim = c(0.95*L, 1.05*U), xaxt = "n",
+     xlab = "forecast horizon", ylab = metric)
+lines(1:H, var_mse, col = col_bvar)
+lines(1:H, pois_mse, col = col_pois)
+lines(1:H, nbinom_mse, col = col_nbinom)
+
+
+
+
+
+
+
+
+
+
+
+h = 1
+t = Tstart
+alpha = 0.1
+yo = Y[t + h, i]
+
+bvar_draws = round(fcast_bvar[h, i, , t])
+dfc_draws = fcast_dfc[h, i, , t]
+pois_draws = fcast_pois[h, i, , t]
+nbinom_draws = fcast_nbinom[h, i, , t]
+
+dfc_med = median(dfc_draws)
+dfc_int = c(quantile(dfc_draws, alpha / 2), quantile(dfc_draws, 1 - alpha/2))
+dfc_crps = scoringRules::crps_sample(yo, dfc_draws)
+
+pois_med = median(pois_draws)
+pois_int = c(quantile(pois_draws, alpha / 2), quantile(pois_draws, 1 - alpha/2))
+pois_crps = scoringRules::crps_sample(yo, pois_draws)
+
+nbinom_med = median(nbinom_draws)
+nbinom_int = c(quantile(nbinom_draws, alpha / 2), quantile(nbinom_draws, 1 - alpha/2))
+nbinom_crps = scoringRules::crps_sample(yo, nbinom_draws)
+
+
+L = min(c(dfc_draws, pois_draws, nbinom_draws))
+U = max(c(dfc_draws, pois_draws, nbinom_draws))
+
+par(mfrow = c(1, 1))
+barplot(table(factor(dfc_draws, levels = L:U)) / dfc_ndraw, names.arg = L:U, border = NA, col = rgb(0, 0, 1, 0.5))
+barplot(table(factor(pois_draws, levels = L:U)) / pois_ndraw, names.arg = L:U, border = NA, add = TRUE, col = rgb(0, 1, 0, 0.5))
+barplot(table(factor(nbinom_draws, levels = L:U)) / nbinom_ndraw, names.arg = L:U, border = NA, add = TRUE, col = rgb(1, 0, 0, 0.5))
+#barplot(table(factor(bvar_draws, levels = L:U)) / bvar_ndraw, names.arg = L:U, border = NA, add = TRUE, col = rgb(0, 0, 0, 0.5))
+
--- simulation_wrongmodel_forecasting.R
+++ simulation_wrongmodel_forecasting.R
@@ -204,16 +204,24 @@ for(t in t0:T){
 # 
 # ==============================================================================
 
-i = 2
+i = 1
 alpha = c(0.05)
 
 dfc_stuff = get_variable_fcasts(i, fcast_dfc)
 var_stuff = get_variable_fcasts(i, fcast_bvar)
 dlm_stuff = get_variable_fcasts(i, fcast_dlm)
 
-dfc_results = forecast_results(Y[(t0 + 1):T, i], dfc_stuff[(t0 + 1):T, , ], alpha)
-var_results = forecast_results(Y[(t0 + 1):T, i], var_stuff[(t0 + 1):T, , ], alpha)
-dlm_results = forecast_results(Y[(t0 + 1):T, i], dlm_stuff[(t0 + 1):T, , ], alpha)
+if(i == 1 & dgp == "varma_copula"){
+  dfc_results = forecast_results_discrete(Y[(t0 + 1):T, i], dfc_stuff[(t0 + 1):T, , ], alpha)
+  var_results = forecast_results_discrete(Y[(t0 + 1):T, i], var_stuff[(t0 + 1):T, , ], alpha)
+  dlm_results = forecast_results_discrete(Y[(t0 + 1):T, i], dlm_stuff[(t0 + 1):T, , ], alpha)
+  metrics = c("MAE", "INT-COV", "INT-SIZE", "CRPS")
+} else {
+  dfc_results = forecast_results(Y[(t0 + 1):T, i], dfc_stuff[(t0 + 1):T, , ], alpha)
+  var_results = forecast_results(Y[(t0 + 1):T, i], var_stuff[(t0 + 1):T, , ], alpha)
+  dlm_results = forecast_results(Y[(t0 + 1):T, i], dlm_stuff[(t0 + 1):T, , ], alpha)
+  metrics = c("MSE", "INT-COV", "INT-SIZE", "CRPS")
+}
 
 dfc_col = "blue"
 var_col = "red"
@@ -224,7 +232,7 @@ png(paste("_images/", dgp, "_forecast_paper.png", sep = ""),
 
 par(mfrow = c(4, 1))
 
-for(metric in c("MSE", "INT-COV", "INT-SIZE", "CRPS")){
+for(metric in metrics){
   if (metric == "CRPS"){
     par(mar = c(4, 4, 0.5, 0.5))
   }else if (metric == "MSE"){
@@ -272,7 +280,7 @@ dev.off()
 # Get results
 # ==============================================================================
 
-i = 2
+i = 1
 alpha = c(0.01, 0.05, 0.1, 0.25, 0.5)
 
 dfc_stuff = get_variable_fcasts(i, fcast_dfc)
@@ -527,5 +535,79 @@ points(1:H, var_crps, col = var_col, pch = 19)
 points(1:H, dlm_crps, col = dlm_col, pch = 19)
 
 box(lwd = box_lwd)
+
+dev.off()
+
+
+
+# ==============================================================================
+# post-process count-valued forecasts
+# ==============================================================================
+
+
+
+i = 1
+alpha = c(0.05)
+
+dfc_stuff = get_variable_fcasts(i, fcast_dfc)
+var_stuff = (get_variable_fcasts(i, fcast_bvar))
+dlm_stuff = (get_variable_fcasts(i, fcast_dlm))
+
+dfc_results = forecast_results_discrete(Y[(t0 + 1):T, i], dfc_stuff[(t0 + 1):T, , ], alpha)
+var_results = forecast_results_discrete(Y[(t0 + 1):T, i], var_stuff[(t0 + 1):T, , ], alpha)
+dlm_results = forecast_results_discrete(Y[(t0 + 1):T, i], dlm_stuff[(t0 + 1):T, , ], alpha)
+metrics = c("MAE", "INT-COV", "INT-SIZE", "CRPS")
+
+
+dfc_col = "blue"
+var_col = "red"
+dlm_col = "orange"
+
+png(paste("_images/", dgp, "poisson_forecast_paper.png", sep = ""), 
+    width = 2, height = 6, units = "in", res = 600)
+
+par(mfrow = c(4, 1))
+
+for(metric in metrics){
+  if (metric == "CRPS"){
+    par(mar = c(2, 4, 0, 0.5))
+  }else if (metric == "MAE"){
+    par(mar = c(0, 4, 0.5, 0.5))
+  }else {
+    par(mar = c(0, 4, 0, 0.5))
+  }
+  
+  dfc_metric = dfc_results[, metric, 1]
+  var_metric = var_results[, metric, 1]
+  dlm_metric = dlm_results[, metric, 1]
+  
+  L = min(dfc_metric, var_metric, dlm_metric)
+  U = max(dfc_metric, var_metric, dlm_metric)
+  
+  plot(1:H, dfc_metric, type = "l", col = dfc_col, ylim = c(L, U), xaxt = "n",
+       xlab = "", ylab = metric, main = "", las = 2)
+  lines(1:H, var_metric, col = var_col)
+  lines(1:H, dlm_metric, col = dlm_col)
+  
+  points(1:H, dfc_metric, col = dfc_col, pch = 19)
+  points(1:H, var_metric, col = var_col, pch = 19)
+  points(1:H, dlm_metric, col = dlm_col, pch = 19)
+  
+  if(metric == "MSE" & dgp == "varma"){
+    legend("bottomright", c("BVAR", "DLM", "DGFC"), lty = 1, bty = "n",
+           col = c(var_col, dlm_col, dfc_col))
+  }
+  
+  if(metric == "CRPS"){
+    axis(1)
+    title(xlab="horizon", line = 2, cex.lab = 1.3)
+    
+  }
+  
+  if(metric == "INT-COV"){
+    abline(h = 1 - alpha, lty = 2, col = "grey")
+  }
+  
+}
 
 dev.off()
